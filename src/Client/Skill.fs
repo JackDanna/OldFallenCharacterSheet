84fbbc0c836140aef69f7b02868207e9aa626662
JackDanna <jackdanna78@gmail.com>
2023-10-15 13:11:19 -0400

    Added skill

---
+++ src/Client/Skill.fs
@@ -0,0 +1,60 @@
+module SkillRow
+
+open Elmish
+open FallenLib.SkillStat
+
+type Model = {
+    skillStat : SkillStat
+    dicePool  : string
+}
+
+type Msg =
+    | Reset
+
+let init () : Model * Cmd<Msg> =
+    let model = {
+        skillStat = emptySkillStat
+        dicePool = ""
+    }
+    let cmd = Cmd.none
+    model, cmd
+
+let update (msg: Msg) (model: Model) : Model * Cmd<Msg> =
+    match msg with
+    | Reset -> init()
+
+open Feliz
+open Feliz.Bulma
+
+let view (model: Model) (dispatch: Msg -> unit) =
+    Bulma.columns [
+        Bulma.column [ prop.text model.skillStat.name ]
+        Bulma.column [ prop.text model.dicePool ]
+        Bulma.column [
+            Html.input [
+                prop.type' "checkbox"
+            ]
+        ]
+        Bulma.column [ prop.text "-" ]
+        Bulma.column [
+            Html.input [
+                prop.type' "checkbox"
+            ]
+        ]
+        Bulma.column [
+            Html.input [
+                prop.type' "checkbox"
+            ]
+        ]
+        Bulma.column [
+            Html.input [
+                prop.type' "checkbox"
+            ]
+        ]
+        Bulma.column [
+            Html.input [
+                prop.type' "checkbox"
+            ]
+        ]
+
+    ]
