c4d2e17649d1270d06a179ec43be22f6f7a8c2e7
JackDanna <jackdanna78@gmail.com>
2023-10-15 18:38:55 -0400

    Made Summed Checkboxes

--- src/Client/StatCheckboxes.fs
+++ src/Client/StatCheckboxes.fs
@@ -1,27 +1,39 @@
 module StatCheckboxes
 
-type Model = bool list
+type Model = {
+    checkBoxes   : bool list
+    numTrueBoxes : int
+}
 
 type Msg = Toggle of int
 
 let update (msg:Msg) (model:Model) : Model =
     match msg with
     | Toggle index ->
-        model
-        |> List.mapi (fun i x -> if i = index then not x else x)
+        let newCheckBoxes = List.mapi (fun i x -> if i = index then not x else x) model.checkBoxes
+        let newNumTrueBoxes = newCheckBoxes |> List.filter id |> List.length
+    
+        { checkBoxes = newCheckBoxes; numTrueBoxes = newNumTrueBoxes }
 
-let init() : Model = List.init 4 (fun _ -> false)
+let init() : Model = {
+    checkBoxes = List.init 4 (fun _ -> false)
+    numTrueBoxes = 0
+}
 
 open Feliz
+open Feliz.Bulma
 
 let view (model : Model) (dispatch : Msg -> unit) =
-    Html.div [
+    Bulma.columns [
         prop.children [
-            for (i, isChecked) in List.indexed model ->
-            Html.input [
-                prop.type' "checkbox"
-                prop.isChecked isChecked
-                prop.onCheckedChange (fun _ -> dispatch (Toggle i))
-            ]
+            for (i, isChecked) in List.indexed model.checkBoxes ->
+                Bulma.column [
+                    Html.input [
+                        prop.type' "checkbox"
+                        prop.isChecked isChecked
+                        prop.onCheckedChange (fun _ -> dispatch (Toggle i))
+                    ]
+                ]
         ]
     ]
+    
