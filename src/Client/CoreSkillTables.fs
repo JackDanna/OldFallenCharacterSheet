0d4645a36ec108a2ff76263afd2eeefa6e1c3ad5
JackDanna <jackdanna78@gmail.com>
2023-10-17 17:41:14 -0400

    Added CoreSKill tables

---
+++ src/Client/CoreSkillTables.fs
@@ -0,0 +1,36 @@
+module CoreSkillTables
+
+type Model = CoreSkillTable.Model list
+
+type Msg =
+    | Modify of int * CoreSkillTable.Msg
+
+let init() : Model = [CoreSkillTable.init();CoreSkillTable.init();CoreSkillTable.init();]
+
+let update (msg: Msg) (model: Model) : Model =
+    match msg with
+    | Modify (position, coreSkillTableMsg) ->
+        model
+        |> List.mapi (fun i coreSkilTableModel ->
+            if i = position then
+                CoreSkillTable.update coreSkillTableMsg coreSkilTableModel
+            else
+                coreSkilTableModel
+        )      
+
+open Feliz
+open Feliz.Bulma
+
+let view (model: Model) (dispatch: Msg -> unit) =
+
+    model
+    |> List.mapi ( 
+        fun position coreSkillTable -> 
+            Bulma.column [
+                CoreSkillTable.view 
+                    coreSkillTable
+                    (fun msg -> dispatch (Modify (position, msg)) )
+            ]
+    )
+    |> Bulma.columns
+
