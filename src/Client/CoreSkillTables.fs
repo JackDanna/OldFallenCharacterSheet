ba792e192077107b60ba63a9ff3789dd28a0f958
JackDanna <jackdanna78@gmail.com>
2023-10-19 13:35:56 -0400

    Added label

--- src/Client/CoreSkillTables.fs
+++ src/Client/CoreSkillTables.fs
@@ -23,21 +23,26 @@ open Feliz.Bulma
 
 let view (model: Model) (dispatch: Msg -> unit) =
 
-    Bulma.columns [
-        columns.isCentered
-        prop.children [
-            model
-            |> List.mapi ( 
-                fun position coreSkillTable -> 
-                    Bulma.column [
-                        CoreSkillTable.view 
-                            coreSkillTable
-                            (fun msg -> dispatch (Modify (position, msg)) )
-                    ]
-            )
-            |> Bulma.columns
+    Bulma.box [
+        Bulma.label [
+            prop.text "Attributes and Core Skills:"
+        ]
+        |> Bulma.content
+        Bulma.columns [
+            columns.isCentered
+            prop.children [
+                model
+                |> List.mapi ( 
+                    fun position coreSkillTable -> 
+                        Bulma.column [
+                            CoreSkillTable.view 
+                                coreSkillTable
+                                (fun msg -> dispatch (Modify (position, msg)) )
+                        ]
+                )
+                |> Bulma.columns
+            ]
         ]
     ]
-    |> Bulma.box
     
 
