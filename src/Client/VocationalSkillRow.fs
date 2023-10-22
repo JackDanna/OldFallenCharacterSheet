46499e3b6f8cc836fe566cc3483cfba5faeba32e
JackDanna <jackdanna78@gmail.com>
2023-10-21 20:27:11 -0400

    Added the Set string logic, still needs some work for the dropdown

--- src/Client/VocationalSkillRow.fs
+++ src/Client/VocationalSkillRow.fs
@@ -13,6 +13,7 @@ type Model = {
 type Msg =
     | VocationalSkillStatMsg of VocationalSkillStat.Msg
     | SetAttributeDiceCalc of DicePoolCalculation
+    | SetName of string
 
 let init() : Model = 
     {
@@ -29,6 +30,7 @@ let update (msg: Msg) (model: Model) : Model =
         { model with VocationalSkillStat = VocationalSkillStat.update vocationalSkillStatMsg model.VocationalSkillStat }
     | SetAttributeDiceCalc attributeDicePoolCalc ->
         { model with attributeDiceCalc = attributeDicePoolCalc }
+    | SetName name -> { model with name = name }
             
         
 open Feliz
@@ -36,7 +38,25 @@ open Feliz.Bulma
 
 let view (model: Model) (dispatch: Msg -> unit) =
     Bulma.columns [
-        Bulma.column [ prop.text model.name ]
+        Bulma.column [
+            Bulma.dropdown [
+                Bulma.dropdownTrigger [
+                    Bulma.input.text [ 
+                        prop.defaultValue model.name
+                        prop.onTextChange (fun value -> dispatch (SetName value) )
+                    ]
+                ]
+                Bulma.dropdownMenu [
+                    Bulma.dropdownContent [
+                        Bulma.dropdownItem.a [
+                            prop.children [
+                                Html.span "Single Sequence"
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
         Bulma.column [
             skillToDicePoolString
                 model.baseDice model.VocationalSkillStat.level model.attributeDiceCalc
