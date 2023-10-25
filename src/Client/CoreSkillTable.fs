c6d63a9fb985f6054cd5204cd92c77b1a9cb7b72
JackDanna <jackdanna78@gmail.com>
2023-10-25 13:26:42 -0400

    Removed Skill Row

--- src/Client/CoreSkillTable.fs
+++ src/Client/CoreSkillTable.fs
@@ -4,37 +4,64 @@ open FallenLib.SkillUtils
 
 type Model = {
     attributeRow  : AttributeRow.Model
-    skillRowList  : SkillRowList.Model
+    skillRowList  : SkillRow.Model list
 }
 
 type Msg =
     | AttributeRowMsg of AttributeRow.Msg
-    | SkillRowListMsg of SkillRowList.Msg
+    | Modify of int * SkillRow.Msg
 
 let init() : Model = {
     attributeRow = AttributeRow.init()
-    skillRowList = SkillRowList.init()
+    skillRowList = []
 }
 
 let update (msg: Msg) (model: Model) : Model =
     match msg with
     | AttributeRowMsg attributeRowMsg ->
-        let newSkillHeaderRow = AttributeRow.update attributeRowMsg model.attributeRow
-        let attributeDicePoolCalc = neg1To4_To_d6_DicePoolCalc newSkillHeaderRow.neg1To4Stat
-        let newSkillList = SkillRowList.update (SkillRowList.Msg.SetAttributeDiceCalc attributeDicePoolCalc) model.skillRowList
+
+        let newAttributeRow = AttributeRow.update attributeRowMsg model.attributeRow
+
+        let attributeRowDiceCalc = neg1To4_To_d6_DicePoolCalc newAttributeRow.neg1To4Stat
+
         { model with 
-            attributeRow = newSkillHeaderRow
-            skillRowList = newSkillList }
+            attributeRow = newAttributeRow
+            skillRowList =
+                List.map (fun skill -> 
+                    SkillRow.update 
+                        (SkillRow.Msg.SetAttributeDiceCalc attributeRowDiceCalc) 
+                        skill
+                ) model.skillRowList
+        }
 
-    | SkillRowListMsg skillRowListMsg ->
-
-        { model with skillRowList = SkillRowList.update skillRowListMsg model.skillRowList }
-
+    | Modify (position, skillRowMsg) ->
+        
+        { model with 
+            skillRowList =
+                List.mapi ( fun i skillRowModel ->
+                    if position = i then
+                        SkillRow.update skillRowMsg skillRowModel
+                    else 
+                        skillRowModel
+                ) model.skillRowList
+        }
+    
 open Feliz
 open Feliz.Bulma
+
+let skillRowList (model: SkillRow.Model list) (dispatch: Msg -> unit) =
+    
+    model
+    |> List.mapi ( 
+        fun position skillRow -> 
+            SkillRow.view 
+                skillRow 
+                (fun msg -> dispatch (Modify (position, msg)) )
+    )
+    |> Html.ul
 
 let view (model: Model) (dispatch: Msg -> unit) =
     Bulma.box [
         AttributeRow.view model.attributeRow (AttributeRowMsg >> dispatch)
-        SkillRowList.view model.skillRowList (SkillRowListMsg >> dispatch)
+        skillRowList model.skillRowList dispatch
     ]
