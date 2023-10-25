763a6de4f094308432bb8c4e4aba8f472dbbaf93
JackDanna <jackdanna78@gmail.com>
2023-10-25 15:08:50 -0400

    Removed VocationalSKillRowList

--- src/Client/VocationTable.fs
+++ src/Client/VocationTable.fs
@@ -2,24 +2,47 @@ module VocationTable
 
 type Model = {
     vocationRow : VocationRow.Model
-    vocationalSkillRowList : VocationalSkillRowList.Model
+    vocationalSkillRowList : VocationalSkillRow.Model List
 }
 
 type Msg =
     | VocationRowMsg of VocationRow.Msg
-    | VocationalSkillRowListMsg of VocationalSkillRowList.Msg
+    | Insert
+    | Remove
+    | Modify of int * VocationalSkillRow.Msg
 
 let init() : Model = {
     vocationRow = VocationRow.init()
-    vocationalSkillRowList = VocationalSkillRowList.init()
+    vocationalSkillRowList = [VocationalSkillRow.init()]
 }
 
 let update (msg: Msg) (model: Model) : Model =
     match msg with
     | VocationRowMsg vocationRowMsg ->
         { model with vocationRow = VocationRow.update vocationRowMsg model.vocationRow }
-    | VocationalSkillRowListMsg vocationalSkillRowListMsg ->
-        { model with vocationalSkillRowList = VocationalSkillRowList.update vocationalSkillRowListMsg model.vocationalSkillRowList }
+    | Insert ->
+        { model with 
+            vocationalSkillRowList = 
+                List.append model.vocationalSkillRowList [VocationalSkillRow.init()]
+        }
+
+    | Remove ->
+        { model with 
+            vocationalSkillRowList = 
+                model.vocationalSkillRowList |> List.rev |> List.tail |> List.rev
+        }
+
+    | Modify (position, skillRowMsg) ->
+
+        { model with 
+            vocationalSkillRowList = 
+                List.mapi ( fun i skillRowModel ->
+                    if position = i then
+                        VocationalSkillRow.update skillRowMsg skillRowModel
+                    else 
+                        skillRowModel
+                ) model.vocationalSkillRowList
+        }
 
 open Feliz
 open Feliz.Bulma
@@ -27,5 +50,23 @@ open Feliz.Bulma
 let view (model: Model) (dispatch: Msg -> unit) =
     Bulma.box [
         VocationRow.view model.vocationRow (VocationRowMsg >> dispatch)
-        VocationalSkillRowList.view model.vocationalSkillRowList (VocationalSkillRowListMsg >> dispatch)
+        Html.ul (
+            List.append (
+                List.mapi ( 
+                    fun position skillRow -> 
+                        VocationalSkillRow.view 
+                            skillRow 
+                            (fun msg -> dispatch (Modify (position, msg)) )
+                ) model.vocationalSkillRowList
+            ) [
+                Html.button [
+                    prop.onClick (fun _ -> dispatch Insert)
+                    prop.text "+"
+                ]
+                Html.button [
+                    prop.onClick (fun _ -> dispatch Remove)
+                    prop.text "-"
+                ]
+            ]
+        )
     ]
