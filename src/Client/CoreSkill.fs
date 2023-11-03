974aba8b0bb7d0d7ad4be4d0d11781ba57c87439
JackDanna <jackdanna78@gmail.com>
2023-11-03 16:03:44 -0400

    Fixed CoreSKills

--- src/Client/CoreSkill.fs
+++ src/Client/CoreSkill.fs
@@ -8,22 +8,28 @@ type Model = SkillStat
 
 type Msg = Neg1To4StatMsg of Neg1To4Stat.Msg
 
-let init () =
-    { name = ""; lvl = Neg1To4Stat.init () }
+let init (attributeLvl: Neg1To4Stat.Model) =
+    let lvl = Neg1To4Stat.init ()
 
-let update (msg: Msg) (model: Model) : Model =
+    { name = ""
+      lvl = lvl
+      dicePool = coreSkillToDicePool baseDicePool lvl attributeLvl }
+
+let update (attributeLvl: Neg1To4Stat.Model) (msg: Msg) (model: Model) : Model =
     match msg with
-    | Neg1To4StatMsg neg1To4StatMsg -> { model with lvl = Neg1To4Stat.update neg1To4StatMsg model.lvl }
+    | Neg1To4StatMsg neg1To4StatMsg ->
+        { model with
+            lvl = Neg1To4Stat.update neg1To4StatMsg model.lvl
+            dicePool = coreSkillToDicePool baseDicePool model.lvl attributeLvl }
 
 open Feliz
 open Feliz.Bulma
 
-let view (attributeLvl: Neg1To4Stat.Model) (model: Model) (dispatch: Msg -> unit) =
+let view (model: Model) (dispatch: Msg -> unit) =
     Bulma.columns [
         Bulma.column [ prop.text model.name ]
         Bulma.column [
-            coreSkillToString baseDicePool model.lvl attributeLvl
-            |> prop.text
+            model.dicePool |> dicePoolToString |> prop.text
         ]
         Bulma.column [
             Neg1To4Stat.view model.lvl (Neg1To4StatMsg >> dispatch)
