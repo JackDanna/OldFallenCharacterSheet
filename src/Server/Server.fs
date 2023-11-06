d99c9f5550442736ebbac94d6908519f39b8933a
JackDanna <jackdanna78@gmail.com>
2023-11-06 16:01:47 -0500

    Removed old Old server stuff

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -6,35 +6,6 @@ open Saturn
 
 open Shared
 
-
-module Storage =
-    let todos = ResizeArray()
-
-    let addTodo (todo: Todo) =
-        if Todo.isValid todo.Description then
-            todos.Add todo
-            Ok()
-        else
-            Error "Invalid todo"
-
-    do
-        addTodo (Todo.create "Create new SAFE project")
-        |> ignore
-
-        addTodo (Todo.create "Write your app") |> ignore
-        addTodo (Todo.create "Ship it !!!") |> ignore
-
-let todosApi =
-    { getTodos = fun () -> async { return Storage.todos |> List.ofSeq }
-      addTodo =
-        fun todo ->
-            async {
-                return
-                    match Storage.addTodo todo with
-                    | Ok () -> todo
-                    | Error e -> failwith e
-            } }
-
 module FallenServerData =
     open FSharp.Data
     open FallenLib.Damage
--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -1,31 +1,14 @@
 namespace Shared
 
-open System
-
 open FallenLib.Item
 open FallenLib.MagicSkill
 open FallenLib.MagicCombat
 open FallenLib.Range
 open FallenLib.CoreSkillGroup
 
-type Todo = { Id: Guid; Description: string }
-
-module Todo =
-    let isValid (description: string) =
-        String.IsNullOrWhiteSpace description |> not
-
-    let create (description: string) =
-        { Id = Guid.NewGuid()
-          Description = description }
-
 module Route =
     let builder typeName methodName =
         sprintf "/api/%s/%s" typeName methodName
-
-type ITodosApi =
-    { getTodos: unit -> Async<Todo list>
-      addTodo: Todo -> Async<Todo> }
-
 
 type IFallenDataApi =
     { getInitData: unit
