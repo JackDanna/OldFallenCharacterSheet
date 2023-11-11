05127803dd42fcc1a9bae4c25ba8c2bd3ddd1218
JackDanna <jackdanna78@gmail.com>
2023-11-11 10:55:38 -0500

    Combined Itemlist with container

--- src/Client/Container.fs
+++ src/Client/Container.fs
@@ -6,7 +6,9 @@ open FallenLib.ContainerClass
 
 type Msg =
     | ToggleIsEquipped of bool
-    | ItemListMsg of ItemList.Msg
+    | ModifyItemRow of int * Item.Msg
+    | Insert of string
+    | Remove of int
 
 let init (containerClas: ContainerClass) : Container =
     { name = ""
@@ -17,11 +19,80 @@ let init (containerClas: ContainerClass) : Container =
 let update (allItemList: Item list) (msg: Msg) (model: Container) : Container =
     match msg with
     | ToggleIsEquipped isEquipped -> { model with isEquipped = isEquipped }
-    | ItemListMsg itemListMsg -> { model with itemList = ItemList.update allItemList itemListMsg model.itemList }
+    | ModifyItemRow (position, itemMsg) ->
+        { model with
+            itemList =
+                model.itemList
+                |> List.mapi (fun index item ->
+                    if position = index then
+                        Item.update allItemList itemMsg item
+                    else
+                        item) }
+    | Insert itemName ->
+        { model with
+            itemList =
+                allItemList
+                |> List.find (fun item -> item.name = itemName)
+                |> List.singleton
+                |> List.append model.itemList
 
+         }
+    | Remove position -> { model with itemList = List.removeAt position model.itemList }
 
 open Feliz
 open Feliz.Bulma
+
+let itemHeaders =
+    [ "Name"
+      "Type"
+      "Tier"
+      //"Dur."
+      "LB"
+      "Value" ]
+
+let itemList (itemNameList: string list) (model: Item list) (dispatch: Msg -> unit) =
+    Bulma.table [
+        table.isBordered
+        prop.children [
+            Html.thead [
+                itemHeaders
+                |> List.map (fun (thString: string) -> Html.th thString)
+                |> Html.tr
+            ]
+            Html.tableBody (
+                List.mapi
+                    (fun position (item: Item) ->
+                        let equipmentRowTableData =
+                            (Item.itemRowColumns itemNameList item (fun msg -> dispatch (ModifyItemRow(position, msg))))
+
+                        let deleteEquipmentRowButton =
+                            Html.td [
+                                Html.button [
+                                    prop.onClick (fun _ -> dispatch (Remove(position)))
+                                    prop.text "-"
+                                ]
+                            ]
+                            |> List.singleton
+
+                        Html.tr (List.append equipmentRowTableData deleteEquipmentRowButton))
+                    model
+            )
+            Html.tfoot [
+                Html.div [
+                    Bulma.input.text [
+                        prop.list "temp"
+                        prop.onTextChange (fun input -> dispatch (Insert input))
+                    ]
+                    Html.datalist [
+                        prop.id "temp"
+                        prop.children (
+                            List.map (fun (itemName: string) -> Html.option [ prop.value itemName ]) itemNameList
+                        )
+                    ]
+                ]
+            ]
+        ]
+    ]
 
 let view (itemNameList: string list) (model: Container) (dispatch: Msg -> unit) =
     Html.div [
@@ -30,5 +101,5 @@ let view (itemNameList: string list) (model: Container) (dispatch: Msg -> unit) 
             prop.value model.isEquipped
             prop.onCheckedChange (fun isChecked -> dispatch (ToggleIsEquipped isChecked))
         ]
-        ItemList.view itemNameList model.itemList (ItemListMsg >> dispatch)
+        itemList itemNameList model.itemList (dispatch)
     ]
