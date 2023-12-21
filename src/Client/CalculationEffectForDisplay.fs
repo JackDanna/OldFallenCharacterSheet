dd703f04bad8675623b1fec8782a9b1356f7cd47
JackDanna <jackdanna78@gmail.com>
2023-12-20 22:33:16 -0500

    Simplified some code

--- src/Client/CalculationEffectForDisplay.fs
+++ src/Client/CalculationEffectForDisplay.fs
@@ -1,18 +1,14 @@
 module MovementSpeedEffectForDisplay
 
-open FallenLib.WeightClass
 open FallenLib.CoreSkillGroup
 open FallenLib.MovementSpeedEffectForDisplay
 
-type Msg =
-    | RecalculateCarryWeight of (CoreSkillGroup list * float * WeightClass list)
-    | RecalculateMovementSpeed of (CoreSkillGroup list * float)
+type Msg = RecalculateMovementSpeed of (CoreSkillGroup list * float)
 
 let update (msg: Msg) (model: MovementSpeedEffectForDisplay) =
     match msg, model with
     | RecalculateMovementSpeed (coreSkillGroupList, percentOfMovementSpeed), model ->
         determineMovementSpeedEffectForDisplay coreSkillGroupList percentOfMovementSpeed model.movementSpeedCalculation
-    | _ -> model
 
 let view (model: MovementSpeedEffectForDisplay) =
     NonInteractiveEffectForDisplay.view (movementSpeedEffectForDisplayToEffectForDisplay model)
